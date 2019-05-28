508c21eeb81cc5a05d67f18376216c2cf12b125b
ermiry <stumpestro@ermiry.com>
2019-05-27 20:02:57 -0500

    Added more utils

--- include/utils.h
+++ include/utils.h
@@ -1,12 +1,32 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <stdbool.h>
+
+/*** Math ***/
+
+extern int clamp_int (int val, int min, int max);
+extern int abs_int (int value);
+extern float lerp (float first, float second, float by);
+
+/*** Random ***/
+
+// init psuedo random generator based on our seed
+extern void random_set_seed (unsigned int seed);
+extern int random_int_in_range (int min, int max);
+
+/*** Converters ***/
+
+// convert a string representing a hex to a string
+extern int xtoi (char *hexString);
+extern char *itoa (int i, char b[]);
 
 /*** STRINGS ***/
 
 extern void str_copy (char *to, char *from);
 extern void str_concat (char *des, char *s1, char *s2);
-extern char *str_toUpper (char *str);
-extern char *str_toLower (char *str);
+extern char *str_to_upper (char *str);
+extern char *str_to_lower (char *str);
 extern char *str_create (const char *stringWithFormat, ...);
 
 /*** OTHER ***/
@@ -16,5 +36,7 @@ extern void clean_stdin (void);
 
 // reads a line from stdin into buffer and returns the lenght
 extern int read_line (char *buffer, int bsize);
+
+extern bool system_is_little_endian (void);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,106 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-/*** STRINGS ***/
+/*** MATH ***/
 
-#pragma region STRINGS
+#pragma region Math
+
+int clamp_int (int val, int min, int max) {
+
+    const int t = val < min ? min : val;
+    return t > max ? max : t;
+
+}
+
+int abs_int (int value) { return value > 0 ? value : (value * -1); }
+
+float lerp (float first, float second, float by) { return first * (1 - by) + second * by; }
+
+#pragma endregion
+
+/*** Random ***/
+
+#pragma region Random
+
+// init psuedo random generator based on our seed
+void random_set_seed (unsigned int seed) { srand (seed); }
+
+int random_int_in_range (int min, int max) {
+
+    int low = 0, high = 0;
+
+    if (min < max) {
+        low = min;
+        high = max + 1;
+    }
+
+    else {
+        low = max + 1;
+        high = min;
+    }
+
+    return (rand () % (high - low)) + low;
+
+}
+
+#pragma endregion
+
+/*** Converters ***/
+
+#pragma region Converters
+
+// convert a string representing a hex to a string
+int xtoi (char *hexString) {
+
+    int i = 0;
+
+    if ((*hexString == '0') && (*(hexString + 1) == 'x')) hexString += 2;
+
+    while (*hexString) {
+        char c = toupper (*hexString++);
+        if ((c < '0') || (c > 'F') || ((c > '9') && (c < 'A'))) break;
+        c -= '0';
+        if (c > 9) c-= 7;
+        i = (i << 4) + c;
+    }
+
+    return i;
+
+}
+
+char *itoa (int i, char b[]) {
+
+    char const digit[] = "0123456789";
+    char *p = b;
+
+    if (i < 0) {
+        *p++ = '-';
+        i *= -1;
+    }
+
+    int shifter = i;
+    do { //Move to where representation ends
+        ++p;
+        shifter = shifter / 10;
+    } while (shifter);
+
+    *p = '\0';
+    do { //Move back, inserting digits as u go
+        *--p = digit [i % 10];
+        i = i / 10;
+    } while (i);
+
+    return b;
+
+}
+
+#pragma endregion
+
+/*** C Strings ***/
+
+#pragma region C Strings
 
 void str_copy (char *to, char *from) {
 
@@ -25,7 +121,7 @@ void str_concat (char *des, char *s1, char *s2) {
 
 }
 
-char *str_toUpper (char *str) {
+char *str_to_upper (char *str) {
 
     if (!str) return NULL;
 
@@ -43,7 +139,7 @@ char *str_toUpper (char *str) {
 
 }
 
-char *str_toLower (char *str) {
+char *str_to_lower (char *str) {
 
     if (!str) return NULL;
 
@@ -92,9 +188,9 @@ char *str_create (const char *stringWithFormat, ...) {
 
 #pragma endregion
 
-/*** OTHER ***/
+/*** Other ***/
 
-#pragma region OTHER
+#pragma region Other
 
 void clean_stdin (void) {
 
@@ -121,6 +217,15 @@ int read_line (char *buffer, int bsize) {
     if (len == bsize - 1) while ((ch = getchar ()) != '\n' && ch != EOF);
 
     return len;
+
+}
+
+bool system_is_little_endian (void) {
+
+    unsigned int x = 0x76543210;
+    char *c = (char *) &x;
+    if (*c == 0x10) return true;
+    else return false;
 
 }
 
