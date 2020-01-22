daa7ad896f46f891b3a6494e14b01021920bb5e5
ermiry <erick_salas@ermiry.com>
2020-01-22 04:29:16 -0600

    Updated string

---
+++ include/string.h
@@ -0,0 +1,49 @@
+#ifndef _TYPES_STRING_H_
+#define _TYPES_STRING_H_
+
+typedef struct String {
+
+    unsigned int len;
+    char *str;
+
+} String;
+
+extern String *str_new (const char *str);
+extern String *str_create (const char *format, ...);
+extern void str_delete (void *str_ptr);
+
+extern void str_copy (String *to, String *from);
+
+extern void str_replace (String *old, const char *str);
+
+// concatenates two strings into a new one
+extern String *str_concat (String *s1, String *s2);
+
+// appends a char to the end of the string
+// reallocates the same string
+extern void str_append_char (String *s, const char c);
+
+// appends a c string at the end of the string
+// reallocates the same string
+extern void str_append_c_string (String *s, const char *c_str);
+
+extern void str_to_upper (String *string);
+extern void str_to_lower (String *string);
+
+extern int str_compare (const String *s1, const String *s2);
+extern int str_comparator (const void *a, const void *b) ;
+
+extern char **str_split (String *string, const char delim, int *n_tokens);
+
+extern void str_remove_char (String *string, char garbage);
+
+// removes the last char from a string
+extern void str_remove_last_char (String *string);
+
+// checks if a string (to_find) is inside string
+// returns 0 on exact match
+// returns 1 if it match the letters but len is different
+// returns -1 if no match
+extern int str_contains (String *string, char *to_find);
+
+#endif
--- src/string.c
+++ src/string.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include <stdarg.h>
 
-#include "myString.h"
+#include "../include/string.h"
 
 static inline void char_copy (char *to, char *from) {
 
@@ -22,8 +22,11 @@ String *str_new (const char *str) {
         if (str) {
             string->len = strlen (str);
             string->str = (char *) calloc (string->len + 1, sizeof (char));
-            char_copy (string->str, (char *) str);
+            if (string->len > 0) char_copy (string->str, (char *) str);
+            else string->str[0] = '\0';
         }
+
+        else string->str = NULL;
     }
 
     return string;
@@ -39,8 +42,8 @@ String *str_create (const char *format, ...) {
 
         va_list argp;
         va_start (argp, format);
-        char oneChar[1];
-        int len = vsnprintf (oneChar, 1, fmt, argp);
+        char one_char[1];
+        int len = vsnprintf (one_char, 1, fmt, argp);
         va_end (argp);
 
         char *str = (char *) calloc (len + 1, sizeof (char));
@@ -59,9 +62,10 @@ String *str_create (const char *format, ...) {
 
 }
 
-void str_delete (String *string) {
+void str_delete (void *str_ptr) {
 
-    if (string) {
+    if (str_ptr) {
+        String *string = (String *) str_ptr;
         if (string->str) free (string->str);
         free (string);
     }
@@ -80,15 +84,74 @@ void str_copy (String *to, String *from) {
 
 }
 
-void str_concat (String *des, String *s1, String *s2) {
+void str_replace (String *old, const char *str) {
 
-    if (des && s1 && s2) {
-        while (*s1->str) *des->str++ = *s1->str++;
-        while (*s2->str) *des->str++ = *s2->str++;
+    if (old && str) {
+        free (old->str);
+        old->len = strlen (str);
+        old->str = (char *) calloc (old->len + 1, sizeof (char));
+        char_copy (old->str, (char *) str);
+    }
 
-        *des->str = '\0';
+}
 
-        des->len = s1->len + s2->len;
+// concatenates two strings into a new one
+String *str_concat (String *s1, String *s2) {
+
+    String *des = NULL;
+
+    if (s1 && s2) {
+        des = str_new (NULL);
+        des->str = (char *) calloc (s1->len + s2->len + 1, sizeof (char));
+        if (des->str) {
+            while (*s1->str) *des->str++ = *s1->str++;
+            while (*s2->str) *des->str++ = *s2->str++;
+
+            *des->str = '\0';
+
+            des->len = strlen (des->str);
+        }
+
+        else {
+            str_delete (des);
+            des = NULL;
+        } 
+    }
+
+    return des;
+
+}
+
+// appends a char to the end of the string
+// reallocates the same string
+void str_append_char (String *s, const char c) {
+
+    if (s) {
+        unsigned int new_len = s->len + 1;   
+
+        s->str = (char *) realloc (s->str, new_len);
+        if (s->str) {
+            char *des = s->str + (s->len);
+            *des = c;
+            s->len = new_len;
+        }
+    }
+
+}
+
+// appends a c string at the end of the string
+// reallocates the same string
+void str_append_c_string (String *s, const char *c_str) {
+
+    if (s && c_str) {
+        unsigned int new_len = s->len + strlen (c_str);
+
+        s->str = (char *) realloc (s->str, new_len);
+        if (s->str) {
+            char *des = s->str + (s->len);
+            char_copy (des, (char *) c_str);
+            s->len = new_len;
+        }
     }
 
 }
@@ -106,6 +169,12 @@ void str_to_lower (String *string) {
 }
 
 int str_compare (const String *s1, const String *s2) { return strcmp (s1->str, s2->str); }
+
+int str_comparator (const void *a, const void *b) {
+
+    if (a && b) return strcmp (((String *) a)->str, ((String *) b)->str);
+
+}
 
 char **str_split (String *string, const char delim, int *n_tokens) {
 
@@ -152,7 +221,7 @@ char **str_split (String *string, const char delim, int *n_tokens) {
 
 }
 
-void str_remove_char (String *string, char *garbage) {
+void str_remove_char (String *string, char garbage) {
 
     char *src, *dst;
     for (src = dst = string->str; *src != '\0'; src++) {
@@ -163,17 +232,34 @@ void str_remove_char (String *string, char *garbage) {
 
 }
 
+// removes the last char from a string
+void str_remove_last_char (String *s) {
+
+    if (s) {
+        if (s->len > 0) {
+            unsigned int new_len = s->len - 1;
+
+            s->str = (char *) realloc (s->str, s->len);
+            if (s->str) {
+                s->str[s->len - 1] = '\0';
+                s->len = new_len;
+            }
+        }
+    }
+
+}
+
+
 int str_contains (String *string, char *to_find) {
 
     int slen = string->len;
     int tFlen = strlen (to_find);
     int found = 0;
 
-    if( slen >= tFlen )
-    {
+    if (slen >= tFlen) {
         for (unsigned int s = 0, t = 0; s < slen; s++) {
             do {
-                if (string->str[s] == to_find[t] ) {
+                if (string->str[s] == to_find[t]) {
                     if (++found == tFlen) return 0;
                     s++;
                     t++;
