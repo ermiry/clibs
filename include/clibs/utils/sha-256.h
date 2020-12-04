27326daed3d17c4105c2f01b7cc4303aeaca07db
ermiry <erick_salas@ermiry.com>
2020-12-03 22:41:50 -0600

    Added base64 & sha256 utilities

---
+++ include/clibs/utils/sha-256.h
@@ -0,0 +1,11 @@
+#ifndef _CLIBS_UTILS_SHA_256_H_
+#define _CLIBS_UTILS_SHA_256_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+extern void sha_256_calc (uint8_t hash[32], const void *input, size_t len);
+
+extern void sha_256_hash_to_string (char string[65], const uint8_t hash[32]);
+
+#endif
