28a83635cfed3b1a581e0b8817f1a9b117e6a833
ermiry <erick.salas@ermiry.com>
2021-01-11 22:51:31 -0600

    Added new utils unit tests

---
+++ test/utils/utils.h
@@ -0,0 +1,10 @@
+#ifndef _CLIBS_TESTS_UTILS_H_
+#define _CLIBS_TESTS_UTILS_H_
+
+extern void utils_tests_base64 (void);
+
+extern void utils_tests_c_strings (void);
+
+extern void utils_tests_sha256 (void);
+
+#endif
