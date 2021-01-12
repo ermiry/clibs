6599bd6071cd659dcc21a8d477f5879b996b71e6
ermiry <erick.salas@ermiry.com>
2021-01-11 23:03:57 -0600

    Added dedicated script to compile sources for benchmarks

---
+++ bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ensure a clean build
+make clean
+
+printf "sources\n\n"
+make TYPE=test NATIVE=1 -j8
+
+printf "\n\ntest\n\n"
+make TYPE=test NATIVE=1 test -j8
+
+printf "\n\nbench\n\n"
+make TYPE=test NATIVE=1 bench -j8
